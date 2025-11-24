2320468b01c052a04fd283d4a3803e007fb0341c
kavyadoshi1504 <kavyadoshi911@gmail.com>
2025-11-24 06:51:31 +0530

    Change socket connection URL to production

--- frontend/src/pages/socket.js
+++ frontend/src/pages/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8000", {
+export const socket = io("https://zylo-y1ys.onrender.com", {
   transports: ["websocket"],
   reconnection: true,
 });
